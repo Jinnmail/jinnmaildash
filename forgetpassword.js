2f7331e1cfe29b21d42c484c3f31650560c56e25
anurag <anuragpurwar007@gmail.com>
2019-01-03 14:30:15 +0530

    added forget password functions

--- forgetpassword.js
+++ forgetpassword.js
@@ -1,21 +1,53 @@
 $(document).ready(() => {
+
+    //let url = 'https://jinnmailapp.herokuapp.com/api/v1/';
+    let url = 'http://localhost:9001/api/v1/'
+
     $('#btn-reset').click(()=>{
-        
         let x= getUrlVars();
-        console.log(x)
+        console.log(x);
+        let password = $('#pass').val();
+        let confirmPassword = $('#confirmPassword').val();
+        console.log(`${password}, ${confirmPassword}`);
+        if(password !== confirmPassword){
+            $('input[type="password"]').css("border", "1px solid red");
+            $('input[type="password"]').css("box-shadow", "0 0 1px red");
+        }
+        else{
+            let data = {
+                email: x.email,
+                token: x.token,
+                password: password
+            }            
+            data = JSON.stringify(data);
+            console.log(data);
+            $.ajax({
+                type: "POST",
+                url: url + 'user/forgot/password/reset',
+                data: data,
+                success: (success) => {
+                    console.log(success)
+                    $(".success-msg").show();                    
+                },
+                error: (err) => {
+                    // alert(err.responseJSON.error)
+                    console.log(err)
+                    $(".credintials-error").text(err.responseJSON.error)
+                    $(".credintials-error").show()
+                },
+                contentType: 'application/json'
+            });
+        }
     });
-
-
     let getUrlVars =  ()=>
         {
-            var vars = [], hash;
-            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-            for(var i = 0; i < hashes.length; i++)
-            {
-                hash = hashes[i].substr(2,hashes[i].length);
-                vars.push(hash[0]);
-                vars[hash[0]] = hash[1];
+            let myURL = new URL(window.location.href);
+            let token = myURL.searchParams.get('t');
+            let email = myURL.searchParams.get('e');
+            let data = {
+                token: atob(token),
+                email: atob(email)
             }
-            return vars;
+            return data;
         }
 })
