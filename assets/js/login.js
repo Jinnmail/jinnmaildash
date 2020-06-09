c2468917f5335f4c350246fd0d0ab67a6147b1e4
JamesSchiller <schillerj78@gmail.com>
2020-06-09 04:15:13 -0400

    logout

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,1 @@
-$(document).ready(function () {
-    // const jinnmailToken = localStorage.getItem('jinnmailToken');
-    // if (jinnmailToken) {
-    //     alert("log out now")
-    // } else {
-    //     window.location.href = 'http://localhost:8000/login.html'
-    // }
-});
+localStorage.removeItem('jinnmailToken');
