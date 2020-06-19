ac6585a0371ffd10e3ba5cbff28c33ab39447a6d
JamesSchiller <schillerj78@gmail.com>
2020-06-18 21:22:39 -0400

    typo

--- constants.js
+++ constants.js
@@ -7,7 +7,7 @@
 // test
 const JM_API_URL = 'https://jinnmailapptest.herokuapp.com/api/v1/';
 const JM_DASHBOARD_URL = 'https://jinnmaildashtest.herokuapp.com/login.html';
-const JM_DASHBOARD_URL_INDEX = 'https://jinnmaildashtest.herokuapp.com/.login.html';
+const JM_DASHBOARD_URL_INDEX = 'https://jinnmaildashtest.herokuapp.com/index.html';
 const JM_EMAIL_DOMAIN='@test.jinnmail.com';
 
 // prod
